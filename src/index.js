b63b69ddec923ab481eec98fa887820ca79e8c8b
isekaimaou1109 <nhat11092000@gmail.com>
2023-01-21 00:47:06 +0700

    Update package.json and main source code

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import { TesseractComponent } from './components/TesseractComponent.vue.js';
 
 const TesseractPlugin = {
   install(Vue) {
-    /** USE AS A PLUGIN IN VUEJS 2 OR 3 */
     Vue.prototype.$tesseractor = () => {
       return Tesseract;
     };
-
-    Vue.component('vue-tesseract', TesseractComponent);
   }
 }
 
